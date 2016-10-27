256f55e7ef6c6bc81d40b94089953e200f359f97
deviantfero <fmorataya.04@gmail.com>
2016-10-27 16:37:46 -0600

    add all least square methods, continuous, discrete and testing

---
+++ leastfun/proc/least.py
@@ -0,0 +1,160 @@
+from sympy import *
+
+class Transformer():
+
+    """Class that contains all attributes and methods
+       Necessary to apply least squares to a function"""
+
+    def __init__(self, var):
+        """
+        :var: Symbol to be used
+
+        """
+        if(var == 'var'):
+            var = 'varn'
+        init_printing()
+        self.var = symbols(str(var)) 
+        self.fx = ""
+        self.ptsx = []
+        self.ptsy = []
+        self.rmat = []
+        self.unsolved_m = []
+        self.cs = []
+        self.eq = 0
+
+    def reset_ans(self):
+        """
+        Clears the answers from the attributes of a transform object
+        """
+        self.rmat = []
+        self.unsolved_m = []
+        self.cs = []
+        self.eq = 0
+
+    
+    def minimize_disc(self, aff):
+        """Gets the least square polinomial according
+        to the aff given
+
+        :aff: the affinity that the aproximate function should have
+        :returns: an expression
+
+        """
+        self.reset_ans()
+        if len(self.ptsx) != len(self.ptsy):
+            if len(self.ptsy) == 1:
+                for val in self.ptsx:
+                    tmp = []
+                    tmp.append( self.ptsy[0].subs(self.var,val) )
+                    self.ptsy = tmp
+                    tmp = []
+            else:
+                raise ValueError('List size invalid')
+
+        for xp in aff:
+            xp = sympify(xp)
+            self.unsolved_m.append( [ xp.subs( self.var, pt ) for pt in self.ptsx ] )
+
+        self.unsolved_m = Matrix(self.unsolved_m)
+
+        for i, xp in enumerate(aff):
+            tmp = []
+            for j, nxp in enumerate(aff):
+                m = self.unsolved_m
+                tmp.append( m.row(i).dot(m.row(j)) )
+            tmp.append( m.row(i).dot( Matrix(self.ptsy)) )
+            self.rmat.append(tmp)
+
+        self.rmat = Matrix(self.rmat).rref()[0]
+
+        self.cs = N(self.rmat.col( self.rmat.cols - 1 ))
+        
+        for i, el in enumerate(aff):
+            self.eq += sympify(el)*self.cs[i]
+
+        return sympify(self.eq)
+
+    def minimize_disc_pot(self):
+        """
+        Gets the least square with the aff = [1, ln(var)]
+        :returns: leastsquare equation where y = exp(leastsquare)
+        """
+        if len(self.ptsy) == 1:
+            for val in self.ptsx:
+                tmp = []
+                tmp.append( log(self.ptsy[0]).subs(self.var,val) )
+                self.ptsy = tmp
+                tmp = []
+        else:
+            self.ptsy = [log(vary) for vary in self.ptsy]
+
+        for x in self.ptsx:
+            if x == 0:
+                raise ValueError('No 0 on LN')
+        return exp(self.minimize_disc([1,'ln('+str(self.var)+')']))
+
+    def minimize_disc_exp(self):
+        """
+        Gets the least square with the aff = [1, var]
+        :returns: leastsquare equation where y = exp(leastsquare)
+        """
+        if len(self.ptsy) == 1:
+            for val in self.ptsx:
+                tmp = []
+                tmp.append( log(self.ptsy[0]).subs(self.var,val) )
+                self.ptsy = tmp
+                tmp = []
+        else:
+            self.ptsy = [log(vary) for vary in self.ptsy]
+
+        return exp(self.minimize_disc([1, self.var]))
+
+    def minimize_cont(self, aff):
+        """Gets the least square polinomial according
+        to the aff given, in a continuous range
+
+        :aff: the affinity that the aproximate function should have
+        :returns: an expression
+
+        """
+        self.reset_ans()
+        tmp = []
+        if len(self.ptsx) != 2:
+            raise ValueError('Wrong range size')
+
+        for xp in aff:
+            xp = sympify(xp)
+            for yp in aff:
+                yp = sympify(yp)
+                tmp.append( integrate(sympify(xp)*sympify(yp), (sympify(self.var), self.ptsx[0], self.ptsx[1])) )
+            tmp.append( integrate(sympify(xp)*sympify(self.fx), (sympify(self.var), self.ptsx[0], self.ptsx[1])) )
+            self.unsolved_m.append(tmp)
+            tmp = []
+
+        self.unsolved_m = Matrix(self.unsolved_m)
+        self.rmat = Matrix(self.unsolved_m).rref()[0]
+
+        self.cs = N(self.rmat.col( self.rmat.cols - 1 ))
+        
+        for i, el in enumerate(aff):
+            self.eq += sympify(el)*self.cs[i]
+
+        return sympify(self.eq)
+
+    def minimize_cont_pot(self):
+        """
+        Gets the least square with the aff = [1, ln(var)]
+        in a continuous range
+        :returns: leastsquare equation where y = exp(leastsquare)
+        """
+        self.fx = 'log('+self.fx+')'
+        return exp(self.minimize_cont([1, log(self.var)]))
+
+    def minimize_cont_exp(self):
+        """
+        Gets the least square with the aff = [1, var]
+        in a continuous range
+        :returns: leastsquare equation where y = exp(leastsquare)
+        """
+        self.fx = 'log('+self.fx+')'
+        return exp(self.minimize_cont([1, self.var]))
---
+++ tests/initial_test.py
@@ -0,0 +1,111 @@
+import os
+import sys
+import unittest
+
+sys.path.append( os.path.join('..', 'leastfun'))
+from proc.least import *
+from sympy import *
+
+varlist = ['x']
+
+class leastTest(unittest.TestCase):
+
+
+    def test_discrete(self):
+        expr = Transformer('vxr')
+        expr.ptsx = [1,2,3,4,5,6]
+        expr.ptsy = [x**2 for x in expr.ptsx]
+        val = expr.minimize_disc([1, 'vxr'])
+        val = val.subs(expr.var,0)
+        self.assertTrue(abs(N(val) + 9.333333) < 10**-4)
+
+    def test_discrete_vars(self):
+        for varn in [ 'x', 'y', 'van', 'voxfm', 'z', 't', 'w' ]:
+            expr = Transformer(varn)
+            expr.ptsx = [1,2,3]
+            expr.ptsy = [cos(x) for x in expr.ptsx]
+            val = expr.minimize_disc([1, varn, varn + '**2'])
+            val = val.subs(varn, 0)
+            self.assertTrue(abs(N(val) - 1.8793593064) < 10**-4)
+
+    def test_discrete_pot(self):
+        for varn in varlist:
+            expr = Transformer(varn)
+            expr.ptsx = [1,2,3]
+            expr.ptsy = [(x**2)+50 for x in expr.ptsx]
+            val = expr.minimize_disc_pot()
+            val = val.subs(varn, 1)
+            self.assertTrue(abs(N(val) - 50.5751542037) < 10**-4)
+
+    def test_discrete_exp(self):
+        for varn in varlist:
+            expr = Transformer(varn)
+            expr.ptsx = [1,2,3]
+            expr.ptsy = [x for x in expr.ptsx]
+            val = expr.minimize_disc_exp()
+            val = val.subs(varn, 1)
+            self.assertTrue(abs(N(val) - 1.04911506342) < 10**-4)
+
+    def test_discrete_custom(self):
+        expr = Transformer('vxr')
+        expr.ptsx = [1,2,3,4,5,6]
+        expr.ptsy = [x**2 for x in expr.ptsx]
+        val = expr.minimize_disc(['vxr', 'vxr**4', 'sqrt(vxr)', '50*vxr**3'])
+        val = val.subs(expr.var,1)
+        self.assertTrue(abs(N(val) - 0.967724515512) < 10**-4)
+
+    def test_discrete_pot_err(self):
+        for varn in varlist:
+            expr = Transformer(varn)
+            expr.ptsx = [1,0,3]
+            expr.ptsy = [cos(x) for x in expr.ptsx]
+            with self.assertRaises(ValueError):
+                val = expr.minimize_disc_pot()
+                val = val.subs(varn, 1)
+
+    def test_cont(self):
+        expr = Transformer('vxr')
+        expr.ptsx = [1,2]
+        expr.fx = "exp(vxr)"
+        val = expr.minimize_cont([1, 'vxr'])
+        val = val.subs(expr.var,0)
+        self.assertTrue(abs(N(val) + 2.22133020755) < 10**-4)
+
+    def test_cont_custom(self):
+        expr = Transformer('vxr')
+        expr.ptsx = [1,2]
+        expr.fx = "exp(vxr)"
+        val = expr.minimize_cont(['vxr', 'vxr**4', 'sqrt(vxr)', '50*vxr**3'])
+        val = val.subs(expr.var,1)
+        self.assertTrue(abs(N(val) - 2.71615115064) < 10**-4)
+
+    def test_cont_vars(self):
+        for varn in [ 'van', 'voxfm' ]:
+            expr = Transformer(varn)
+            expr.ptsx = [1,2]
+            expr.fx = 'exp('+varn+')'
+            val = expr.minimize_cont([1, varn])
+            val = val.subs(expr.var,0)
+            self.assertTrue(abs(N(val) + 2.22133020755) < 10**-4)
+
+    def test_cont_exp(self):
+        expr = Transformer('vxr')
+        expr.ptsx = [1,2]
+        expr.fx = "vxr**2"
+        val = expr.minimize_cont_exp()
+        val = val.subs(expr.var,1)
+        self.assertTrue(abs(N(val) - 1.09456266299) < 10**-4)
+
+    def test_cont_pot(self):
+        expr = Transformer('vxr')
+        expr.ptsx = [1,2]
+        expr.fx = "vxr**2"
+        val = expr.minimize_cont_pot()
+        val = val.subs(expr.var,2)
+        self.assertTrue(abs(N(val) - 4) < 10**-4)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
