a4c5aa2c73fb28ca4505803fc5dd0fcab52aca68
deviantfero <fmorataya.04@gmail.com>
2016-11-27 16:26:17 -0600

    fixes to installer

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ function install_dependencies {
 		version="$( uname -a | grep -Eo "(eneric|bian)" )"
 		if [ -n "$version" ]; then
 			echo ":: DEBIAN OR *BUNTU DETECTED"
-			sh -c "sudo apt-get install feh python3-gi python-gobject python-pip python-imaging python-matplotlib python-sympy && pip install Pillow"
+			sh -c "sudo apt-get install feh python3-gi python-gobject python3-pip python-imaging && pip3 install matplotlib sympy tk"
 		else
 			echo ":: ANOTHER DISTRO DETECTED:: INSTALL DEPENDENCIES FOR YOUR DISTRO"
 			echo
